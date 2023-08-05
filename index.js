e5089f1c3056cac5f15ae0abd3414974b61443e4
Sathvik Reddy <sathvik5883@gmail.com>
2023-08-05 14:59:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const inputBox = document.getElementById("task-input");
+const listContainer = document.getElementById("list-container");
+const btnEl = document.querySelector(".btn");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert('You need to type something !!!');
+    }else{
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        listContainer.appendChild(li).appendChild(span);
+    }
+    inputBox.value = '';
+    saveData();
+}
+
+btnEl.addEventListener('click',addTask);
+
+listContainer.addEventListener('click',(e) => {
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+        saveData();
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+    }
+    saveData();
+})
+
+function saveData(){
+    localStorage.setItem('data',listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+
+showTask();
